0bc202fa45624169627fdc8721b0166d6b152fa0
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-05-21 16:44:46 +0900

    initial commit

--- 백준_누적합/인간-컴퓨터 상호작용/src/Main.java
+++ 백준_누적합/인간-컴퓨터 상호작용/src/Main.java
@@ -5,28 +5,24 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static void getCount(int[][] arr, String s){
-        for(int i=1; i<=s.length(); i++){
-            for(int j=0; j<26; j++){
-                arr[i][j] = arr[i-1][j];
-            }
-            int num = s.charAt(i-1) - 'a';
-            arr[i][num]++;
-        }
-    }
-
     public static void main(String[] args)throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
         int[][] arr = new int[s.length()+1][26];
-        getCount(arr, s);
+        for(int i=1; i<=s.length(); i++){
+            int tmp = s.charAt(i-1)-'a';
+            for(int j=0; j<26; j++){
+                arr[i][j] = arr[i-1][j];
+            }
+            arr[i][tmp]++;
+        }
         int T = Integer.parseInt(br.readLine());
         for(int i=0; i<T; i++){
             StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-            int x = st.nextToken().charAt(0) - 'a';
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            System.out.println(arr[end+1][x] - arr[start][x]);
+            int c = st.nextToken().toCharArray()[0] - 'a';
+            int l = Integer.parseInt(st.nextToken());
+            int r = Integer.parseInt(st.nextToken());
+            System.out.println(arr[r+1][c] - arr[l][c]);
         }
     }
 }
