54fd5602dbc4c2bddeba72963ebb627d77bd3def
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-06-04 15:00:31 +0900

    initial commit

--- 백준_분할 정복/행렬 곱셈/src/Main.java
+++ 백준_분할 정복/행렬 곱셈/src/Main.java
@@ -4,30 +4,51 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[][] arr1;
-    static int[][] arr2;
 
-    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-
-    static void getArray(int a, int b, int[][] arr)throws IOException{
-        st  =new StringTokenizer(br.readLine(), " ");
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int a, m, k;
+        int[][] arr1 = new int[128][128];
+        int[][] arr2 = new int[128][128];
+        st = new StringTokenizer(br.readLine(), " ");
         a = Integer.parseInt(st.nextToken());
-        b = Integer.parseInt(st.nextToken());
-        arr = new int[a][b];
+        m = Integer.parseInt(st.nextToken());
         for(int i=0; i<a; i++){
             st = new StringTokenizer(br.readLine(), " ");
-            for(int j=0; j<b; j++){
-                arr[i][j] = Integer.parseInt(st.nextToken());
+            for(int j=0; j<m; j++){
+                arr1[i][j] = Integer.parseInt(st.nextToken());
             }
+        }
+        st = new StringTokenizer(br.readLine(), " ");
+        m = Integer.parseInt(st.nextToken());
+        k = Integer.parseInt(st.nextToken());
+        for(int i=0; i<m; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j=0; j<k; j++){
+                arr2[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        int[][] arr3 = solution(arr1, arr2);
+        for(int i=0; i<a; i++){
+            for(int j=0; j<k; j++){
+                System.out.print(arr3[i][j] + " ");
+            }
+            System.out.println();
         }
     }
 
-    public static void main(String[] args)throws IOException {
-        int n = 0, m= 0, k= 0;
-        getArray(n, m, arr1);
-        getArray(m, k, arr2);
-        int[][] arr3 = new int[n][k];
+    public static int[][] solution(int[][] arr1, int[][] arr2) {
+        int[][] answer = new int[arr1.length][arr2[0].length];
 
+        for (int i = 0; i < arr1.length; ++i) {
+            for (int j = 0; j < arr2[0].length; ++j) {
+                for (int k = 0; k < arr1[0].length; ++k) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
+                }
+            }
+        }
+
+        return answer;
     }
 }
---
+++ 백준_분할 정복/행렬 제곱/src/Main.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br  = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int n = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        long[][] arr = new long[n][n];
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine(), " ");
+            for(int j=0; j<n; j++) {
+                arr[i][j] = Long.parseLong(st.nextToken());
+            }
+        }
+        long[][] result = PoweredMatrix(n, b, arr);
+        for(int i=0; i<n; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(result[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static long[][] PoweredMatrix(int n, int b, long[][] arr){
+        if(b == 2) return solution(arr, arr);
+        else{
+            if(b%2 == 0){
+                return solution(PoweredMatrix(n, b/2, arr), PoweredMatrix(n, b/2, arr));
+            }
+            else {
+                return solution(PoweredMatrix(n, b-1, arr), arr);
+            }
+        }
+    }
+
+    public static long[][] solution(long[][] arr1, long[][] arr2) {
+        long[][] answer = new long[arr1.length][arr2[0].length];
+        for (int i = 0; i < arr1.length; ++i) {
+            for (int j = 0; j < arr2[0].length; ++j) {
+                for (int k = 0; k < arr1[0].length; ++k) {
+                    answer[i][j] = (answer[i][j] + arr1[i][k] * arr2[k][j])%1000;
+                }
+            }
+        }
+        return answer;
+    }
+}
