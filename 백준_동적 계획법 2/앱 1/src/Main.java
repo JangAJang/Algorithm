f0c89aa8916d9919a059b358c3042c50e7825167
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-06-21 09:36:51 +0900

    initial commit

--- 백준_동적 계획법 2/앱 1/src/Main.java
+++ 백준_동적 계획법 2/앱 1/src/Main.java
@@ -12,14 +12,24 @@ public class Main {
         int[] arr = new int[n+1];
         int[] c = new int[n+1] ;
         long[][] dp = new long[n+1][10001];
+        int result=Integer.MAX_VALUE;
         st =new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st2 = new StringTokenizer(br.readLine(), " ");
         for(int i=1; i<=n; i++){
             arr[i] = Integer.parseInt(st.nextToken());
+            c[i] = Integer.parseInt(st2.nextToken());
         }
-        st = new StringTokenizer(br.readLine(), " ");
         for(int i=1; i<=n; i++){
-            c[i] = Integer.parseInt(st.nextToken());
+            for(int j=0; j<= 10000; j++){
+                if(i==1 && j>= c[i]) dp[i][j] = arr[i];
+                else{
+                    if(j >= c[i]) dp[i][j] = Math.max(dp[i-1][j], dp[i-1][j-c[i]] + arr[i]);
+                    else dp[i][j] = dp[i-1][j];
+                }
+                if(dp[i][j] >= m) result = Math.min(j, result);
+            }
         }
+        System.out.println(result);
         
     }
 }
