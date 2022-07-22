2d64cd214b2d519500eecbecf680d27c1116a734
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-07-23 08:49:30 +0900

    initial commit

--- 백준_그래프와 순회/벽부수기/src/Main.java
+++ 백준_그래프와 순회/벽부수기/src/Main.java
@@ -1,79 +1,86 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
+import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
+
 
 public class Main {
+    static class Loc{
+        int i;
+        int j;
+        int cnt;
+        boolean destroyed;
 
-    static int[][] arr;
-    static int N;
-    static int M;
-    static int[][] count;
-    static boolean[][] visit;
-    static Queue<Dot> ones;
-
-    public static void main(String[] argsh)throws IOException{
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        arr = new int[N+1][M+1];
-        visit = new boolean[N+1][M+1];
-        ones = new ArrayDeque<>();
-        for(int i=1; i<=N; i++){
-            String s = br.readLine();
-            for(int j=1; j<=M; j++){
-                arr[i][j] = Character.getNumericValue(s.charAt(j-1));
-                if(arr[i][j] == 1) ones.add(new Dot(i, j));
-            }
+        public Loc(int i, int j, int cnt, boolean d) {
+            this.i = i;
+            this.j = j;
+            this.cnt = cnt;
+            this.destroyed = d;
         }
-        bfs();
     }
 
-    static void bfs(){
-        int[] x_area = {-1, 1, 0, 0};
-        int[] y_area = {0, 0, -1, 1};
-        while(!ones.isEmpty()){
-            Dot delete = ones.poll();
-            Queue<Dot> queue = new ArrayDeque<>();
-            queue.add(new Dot(1, 1));
-            visit[1][1] = true;
-            count= new int[N+1][M+1];
-            count[delete.getX()][delete.getY()] = -1;
-            while(!queue.isEmpty()){
-                Dot tmp = queue.poll();
-                for(int i=0; i<4; i++){
-                    int x = tmp.getX() + x_area[i];
-                    int y = tmp.getY() + y_area[i];
-                    if(x >=1 && y>=1 && x<=N && y<= M){
-                        if(arr[x][y] == 0 && !visit[x][y]){
-                            queue.add(new Dot(x, y));
-                            visit[x][y] = true;
-                            count[x][y] = count[tmp.getX()][tmp.getY()] + 1;
-                        }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] inputs = br.readLine().split(" ");
+
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
+
+        char[][] map = new char[n][m];
+        for (int i = 0; i < n; i++) {
+            String input = br.readLine();
+            for (int j = 0; j < m; j++) {
+                map[i][j] = input.charAt(j);
+            }
+        }
+
+
+        Queue<Loc> q = new LinkedList<>();
+        q.add(new Loc(0, 0, 1, false));
+
+        int[] mi = {0, 0, -1, 1};
+        int[] mj = {-1, 1, 0, 0};
+
+        boolean[][][] visited = new boolean[n][m][2];
+
+        while (!q.isEmpty()) {
+            Loc now = q.poll();
+
+            if (now.i == n - 1 && now.j == m - 1) {
+                System.out.println(now.cnt);
+                return;
+            }
+
+            for (int d = 0; d < 4; d++) {
+                int ni = now.i + mi[d];
+                int nj = now.j + mj[d];
+
+                if(ni<0 || ni>=n || nj<0 || nj>=m) continue;
+
+                int next_cnt = now.cnt+1;
+
+                if(map[ni][nj]=='0'){ 
+                    if(!now.destroyed && !visited[ni][nj][0]) {
+                        q.add(new Loc(ni, nj, next_cnt, false));
+                        visited[ni][nj][0] = true;
+                    }else if(now.destroyed && !visited[ni][nj][1]){
+                        q.add(new Loc(ni, nj, next_cnt, true));
+                        visited[ni][nj][1] = true;
+                    }
+
+                }else if(map[ni][nj]=='1'){
+
+                    if(!now.destroyed){
+                        q.add(new Loc(ni, nj, next_cnt, true));
+                        visited[ni][nj][1] = true;
                     }
                 }
             }
-        }
-        System.out.println(count[N][M]);
-    }
-    static class Dot{
-        int x;
-        int y;
 
-        public Dot(int x, int y) {
-            this.x = x;
-            this.y = y;
         }
 
-        public int getX() {
-            return x;
-        }
-
-        public int getY() {
-            return y;
-        }
+        System.out.println(-1);
     }
 }
