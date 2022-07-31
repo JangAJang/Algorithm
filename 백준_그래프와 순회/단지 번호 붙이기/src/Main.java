01fdf9d71b49be0bfaefc28a3bcfae2edf8adfce
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-07-31 09:12:01 +0900

    initial commit

--- 백준_그래프와 순회/단지 번호 붙이기/src/Main.java
+++ 백준_그래프와 순회/단지 번호 붙이기/src/Main.java
@@ -1,57 +1,75 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.HashMap;
+import java.util.*;
+import java.io.*;
 
 public class Main {
+    static int arr[][];
+    static boolean visit[][];
+    static int dirX[] = {0, 0, -1, 1};
+    static int dirY[] = {-1, 1, 0, 0};
 
-    static int[][] arr;
-    static boolean[][] visitied;
-    static int count = 0;
-    static int n;
-    static int[] result;
+    static int count = 0, number = 0;
+    static int nowX, nowY, N;
 
-    public static void main(String[] args)throws IOException{
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        arr = new int[n+1][n+1];
-        visitied = new boolean[n+1][n+1];
-        result = new int[(n+1)*(n+1)];
-        for(int i=1; i<=n; i++){
-            String s = br.readLine();
-            for(int j=1; j<=n; j++){
-                arr[i][j] = Character.getNumericValue(s.charAt(j-1));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        List<Integer> list = new ArrayList<>();
+
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+        visit = new boolean[N][N];
+
+        for(int i=0; i<N; i++) {
+            String str = br.readLine();
+
+            for(int j=0; j<N; j++) {
+                arr[i][j] = Character.getNumericValue(str.charAt(j));
             }
         }
-        for(int i=1; i<=n; i++){
-            for(int j=1; j<=n; j++){
-                if(arr[i][j] == 1 && !visitied[i][j]){
-                    count++;
-                    dfs(i, j);
+
+        for(int i=0; i<N; i++) {
+            for(int j=0; j<N; j++) {
+
+                if(visit[i][j] == false && arr[i][j] == 1) {
+                    count = 0;
+                    number++;
+                    DFS(i, j);
+                    list.add(count);
                 }
+
             }
         }
-        Arrays.sort(result);
-        System.out.println(count);
-        for(int i=0; i<result.length; i++){
-            if(result[i]!=0) System.out.println(result[i]);
+
+        Collections.sort(list);
+        bw.append(number + "\n");
+        for(int num : list) {
+            bw.append(num + "\n");
         }
+
+        bw.flush();
+        bw.close();
+    } 
+
+    static void DFS(int x, int y) {
+        visit[x][y] = true;
+        arr[x][y] = number;
+        count ++;
+
+        for(int i=0; i<4; i++) {
+            nowX = dirX[i] + x;
+            nowY = dirY[i] + y;
+
+            if(Range_check() && visit[nowX][nowY] == false && arr[nowX][nowY] == 1) {
+                visit[nowX][nowY] = true;
+                arr[nowX][nowY] = number;
+
+                DFS(nowX, nowY);
+            }
+        }
+
     }
 
-    public static void dfs(int x, int y){
-        visitied[x][y] = true;
-        result[count]++;
-        int dx[] = {0,0,1,-1};
-        int dy[] = {1,-1,0,0};
-        for(int i=0; i<4; i++){
-            int finalX = x + dx[i];
-            int finalY = x + dy[i];
-            if(finalX >=0 && finalY >=0 && finalX <= n && finalY <= n){
-                if(arr[finalX][finalY] == 1 && !visitied[finalX][finalY]){
-                    dfs(finalX,finalY);
-                }
-            }
-        }
+    static boolean Range_check() {
+        return (nowX >= 0 && nowX < N && nowY >= 0 && nowY < N);
     }
 }
