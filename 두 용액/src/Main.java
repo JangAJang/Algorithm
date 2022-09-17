5f57e1369ecb1c95817edea120c942ede1b7b251
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-09-17 15:46:20 +0900

    initial commit

---
+++ 두 용액/src/Main.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        for(int i=0; i<N; i++){
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+        for(int i=0; i<N; i++){
+            System.out.println(arr[i]);
+        }
+        int start = 0;
+        int end = N-1;
+        int min = Integer.MAX_VALUE;
+        int x = 0, y = 0;
+        while(start < end){
+            int sum = arr[start] + arr[end];
+            if(sum < min){
+                min = sum;
+                System.out.println(min);
+                x = start;
+                y = end;
+            }
+            if(sum <= min){
+                start++;
+            }
+            else end--;
+        }
+        System.out.println(arr[x] + " " + arr[y]);
+    }
+}
