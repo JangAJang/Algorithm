db3d9bc0d9af82486d8d2790ed96a3fea598da1b
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-09-29 21:41:08 +0900

    initial commit

--- 백준_스택 2/오등큰수/src/Main.java
+++ 백준_스택 2/오등큰수/src/Main.java
@@ -8,11 +8,10 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N+1];
-        Stack<Integer> stack = new Stack<>();
+        int[] arr = new int[N];
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
         HashMap<Integer, Integer> hashMap = new HashMap<>();
-        for(int i=1; i<=N; i++){
+        for(int i=0; i<N; i++){
             int x = Integer.parseInt(st.nextToken());
             arr[i] = x;
             if(hashMap.containsKey(x)){
@@ -20,22 +19,34 @@ public class Main {
             }
             else hashMap.put(x, 1);
         }
-        for(int i=N; i>=1; i--){
-            stack.push(arr[i]);
-        }
-        for(int i=1; i<=N; i++){
-            int x = stack.pop();
-            if(stack.isEmpty()) bw.write("-1");
-            else{
-                int ngf = -1;
-                for(int j=i+1; j<=N; j++){
-                    if(hashMap.get(arr[j]) > hashMap.get(x)){
-                        ngf = arr[j];
-                        break;
-                    }
-                }
-                bw.write(ngf + " ");
+        Stack<Integer> tmp = new Stack<>();
+        Stack<Integer> result = new Stack<>();
+        for(int i=N-1; i>=0; i--){
+            if(!tmp.isEmpty() && hashMap.get(tmp.peek()) > hashMap.get(arr[i])){
+                result.push(tmp.peek());
+                tmp.push(arr[i]);
             }
+            else if(!tmp.isEmpty() && hashMap.get(tmp.peek()) <= hashMap.get(arr[i])){
+                while( !tmp.isEmpty() && hashMap.get(tmp.peek()) <= hashMap.get(arr[i])){
+                    tmp.pop();
+                }
+                if(tmp.isEmpty()) {
+                    result.push(-1);
+                    tmp.push(arr[i]);
+                }
+                else{
+                    result.push(tmp.peek());
+                    tmp.push(arr[i]);
+                }
+            }
+            else{
+                result.push(-1);
+                tmp.push(arr[i]);
+            }
+        }
+        for(int i=0; i<N; i++){
+            int temp = result.pop();
+            bw.write(temp+" ");
         }
         bw.flush();
     }
