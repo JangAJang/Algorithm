6b7716d35b7356afd37347bb3c0eefe8380bcd12
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-10-02 23:06:37 +0900

    initial commit

---
+++ 백준_최단경로/Oasis/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.Stack;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] arg)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        long[] arr = new long[N];
+        long count = 0;
+        Stack<Long> tmp = new Stack<>();
+        for(int i=0; i<N; i++){
+            arr[i] = Long.parseLong(br.readLine());
+        }
+        for(int i=0; i<N; i++){
+            
+        }
+    }
+}
