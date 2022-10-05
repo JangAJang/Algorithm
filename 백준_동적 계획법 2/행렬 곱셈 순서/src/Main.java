f9b8e7696c6a96bce41757aea4dfc557aecd76a5
JangAJang <99702271+JangAJang@users.noreply.github.com>
2022-10-05 18:05:14 +0900

    initial commit

--- 백준_동적 계획법 2/행렬 곱셈 순서/src/Main.java
+++ 백준_동적 계획법 2/행렬 곱셈 순서/src/Main.java
@@ -4,27 +4,41 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+
+    public static long[][] dp;
+    public static int[][] arr;
+
+    public static void main(String[] args)throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int count = Integer.parseInt(br.readLine());
-        int[] data = new int[count+1];
-        StringTokenizer st;
-        for(int i=0; i<count; i++){
-            st = new StringTokenizer(br.readLine(), " ");
-            data[i] = Integer.parseInt(st.nextToken());
-            data[i+1] = Integer.parseInt(st.nextToken());
+        int length = Integer.parseInt(br.readLine());
+        arr = new int[length][2];
+        dp = new long[length][length];
+        for(int i=0; i<length; i++){
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            arr[i][0] = x;
+            arr[i][1] = y;
 
         }
-        int[][] dp = new int[count][count];
-        for(int i=2; i<count+1; i++){
-            for(int j=0; j<count-1+i; j++){
-                dp[i][j+i-1] = Integer.MAX_VALUE;
-                for(int k=j; k<j+i-1; k++){
-                    int value = dp[j][k]  + dp[k+1][j+i-1] + (data[j]*data[k+1]*data[j+i]);
-                    dp[j][j+i-1] = Math.min(dp[j][j+i-1], value);
-                }
+        for(int i=0; i<length; i++){
+            for(int j=0; j<length; j++){
+                if(i==j) dp[i][j] = 0;
+                else dp[i][j] = Long.MAX_VALUE;
             }
         }
-        System.out.println(dp[0][count-1]);
+        for(int i=1; i<length; i++){
+            for(int j=0; j+i<length; j++){
+                calculate(j, j+i);
+            }
+        }
+        System.out.println(dp[0][length-1]);
+    }
+
+    public static void calculate(int start, int end){
+        for(int i= start; i< end; i++){
+            long cost = dp[start][i] + dp[i+1][end] + (long) arr[start][0] * arr[i][1] * arr[end][1];
+            dp[start][end] = Math.min(dp[start][end], cost);
+        }
     }
 }
