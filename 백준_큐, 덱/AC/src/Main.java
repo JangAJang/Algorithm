cdcd5f28bad7bf89c265d82d0e35f65e85bae963
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-01-16 16:40:08 +0900

    initial commit

--- 백준_큐, 덱/AC/src/Main.java
+++ 백준_큐, 덱/AC/src/Main.java
@@ -7,30 +7,43 @@ import java.util.stream.Collectors;
 public class Main {
 
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static void main(String[] args)throws IOException {
-        int tasks = Integer.parseInt(br.readLine());
-        while(tasks-- > 0){
+
+    public static void main(String[] args) throws IOException {
+        int count = Integer.parseInt(br.readLine());
+        while(count-->0){
             try{
-                checkEachProcess();
-            }catch (Exception e){
+                System.out.println(modifyArray());
+            }catch(NumberFormatException e){
                 System.out.println("error");
             }
         }
     }
 
-    private static void checkEachProcess()throws IOException {
-        String command = br.readLine();
-        int number = Integer.parseInt(br.readLine());
-        List<Integer> numberArray = Arrays.stream(br.readLine().replace("[", "").replace("]", "")
-                .split(",")).mapToInt(Integer::parseInt).boxed().collect(Collectors.toList());
-        for(int index = 0; index < command.length(); index++){
-            changeWithCommand(command.charAt(index), numberArray);
-        }
-        System.out.println(numberArray.toString());
+    private static String modifyArray()throws IOException{
+        String commands = br.readLine();
+        int length = Integer.parseInt(br.readLine());
+        Deque<Integer> numbers = Arrays.stream(br.readLine()
+                .replace("[", "").replace("]", "")
+                .split(",")).mapToInt(Integer::parseInt).boxed()
+                .collect(Collectors.toCollection(ArrayDeque::new));
+        return activateCommands(commands, numbers);
     }
 
-    private static void changeWithCommand(char commandEach, List<Integer> numberArray){
-        if(commandEach == 'R') Collections.reverse(numberArray);
-        if(commandEach == 'D') numberArray.remove(0);
+    private static String activateCommands(String commands, Deque<Integer> numbers){
+        for(int index = 0; index < commands.length(); index++){
+            if(commands.charAt(index) == 'D' && numbers.isEmpty()) return "error";
+            numbers = adjustEachCommand(commands.charAt(index), numbers);
+        }
+        return numbers.toString();
+    }
+
+    private static Deque<Integer> adjustEachCommand(char command, Deque<Integer> numbers){
+        if(command == 'R'){
+            List<Integer> flip = new ArrayList<>(numbers);
+            Collections.reverse(flip);
+            return new ArrayDeque<>(flip);
+        }
+        numbers.pollFirst();
+        return numbers;
     }
 }
