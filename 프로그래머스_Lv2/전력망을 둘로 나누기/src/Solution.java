5f4248ee66789c59987ddf0e566f011a2bec91d1
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-10 16:33:38 +0900

    refactor: Solution 기능별로 메서드 분리

--- 프로그래머스_Lv2/전력망을 둘로 나누기/src/Solution.java
+++ 프로그래머스_Lv2/전력망을 둘로 나누기/src/Solution.java
@@ -1,9 +1,20 @@
 import java.util.*;
 
 class Solution {
+
+    private HashMap<Integer, Set<Integer>> map;
+
     public int solution(int n, int[][] wires) {
+        createGraph(n,wires);
         int result = Integer.MAX_VALUE;
-        HashMap<Integer, Set<Integer>> map = new HashMap<>();
+        for(int[] wire : wires){
+            result = Math.min(result, Math.abs(n - 2 * countNode(wire)));
+        }
+        return result;
+    }
+
+    private void createGraph(int n, int[][] wires){
+        map = new HashMap<>();
         for(int index = 1; index <=n; index++){
             map.put(index, new HashSet<>());
         }
@@ -11,26 +22,21 @@ class Solution {
             map.get(wire[0]).add(wire[1]);
             map.get(wire[1]).add(wire[0]);
         }
-        for(int[] wire : wires){
-            result = Math.min(result, Math.abs(n - 2 * countNode(map, wire)));
-        }
-        return result;
     }
 
-    private int countNode(HashMap<Integer, Set<Integer>> map, int[] wire){
+    private int countNode(int[] wire){
         Stack<Integer> stack = new Stack<>();
         boolean[] visited = new boolean[map.keySet().size()+1];
         stack.add(1);
         while(!stack.isEmpty()){
             int current = stack.pop();
+            addNextStations(wire, current, visited, stack);
             visited[current] = true;
-            for(int next : map.get(current)){
-                if(!visited[next] && !isSkip(wire, current, next)){
-                    stack.push(next);
-                    visited[next] = true;
-                }
-            }
         }
+        return countVisits(visited);
+    }
+
+    private int countVisits(boolean[] visited){
         int result = 0;
         for(boolean visit : visited){
             if(visit) result++;
@@ -38,8 +44,17 @@ class Solution {
         return result;
     }
 
-    private boolean isSkip(int[] wire, int current, int next) {
-        return (wire[0] == current && wire[1] == next) ||
-                (wire[1] == current && wire[0] == next);
+    private void addNextStations(int[] wire, int current, boolean[] visited, Stack<Integer> stack){
+        for(int next : map.get(current)){
+            if(!visited[next] && !isSkip(wire, new int[]{current, next})){
+                stack.push(next);
+                visited[next] = true;
+            }
+        }
+    }
+
+    private boolean isSkip(int[] wire, int[] currentWire) {
+        return (wire[0] == currentWire[0] && wire[1] == currentWire[1]) ||
+                (wire[1] == currentWire[0] && wire[0] == currentWire[1]);
     }
 }
