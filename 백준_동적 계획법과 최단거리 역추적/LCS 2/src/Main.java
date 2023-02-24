279df0151474a01ea4ede3e082a1d69e5d5d7af0
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-02-24 20:59:05 +0900

    initial commit

--- 백준_동적 계획법과 최단거리 역추적/LCS 2/src/Main.java
+++ 백준_동적 계획법과 최단거리 역추적/LCS 2/src/Main.java
@@ -1,83 +1,53 @@
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStreamWriter;
-
+import java.util.*;
+import java.io.*;
+import java.util.stream.*;
 
 public class Main {
 
+    private static Solution solution = new Solution();
 
-    static int[][] dp = new int[1001][1001];
-    static String[] n1;
-    static String[] n2;
-
-    public static void main(String[] args) throws IOException{
-
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-
-        n1 = br.readLine().split("");
-        n2 = br.readLine().split("");
-        String ans = solve();
-
-        bw.write(ans.length() + "\n");
-        if(ans.length()!=0 ) bw.write(ans + "\n");
-
-
-        bw.flush();
-        bw.close();
+    public static void main(String[] args) throws Exception{
+        solution.solution();
     }
 
+    static class Solution{
 
+        private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
-    public static String solve() {
-
-        // 테이블 그리기
-        for(int i=0; i<n1.length; i++) {
-            for(int j=0; j<n2.length; j++) {
-                if(n1[i].equals(n2[j])) {
-                    dp[i+1][j+1] = dp[i][j]+1;
+        public void solution()throws Exception{
+            String s1 = br.readLine();
+            String s2 = br.readLine();
+            int[][] count = new int[s1.length()+1][s2.length()+1];
+            IntStream.range(0, s1.length()+1).forEach(i-> count[i][0] = 0);
+            IntStream.range(0, s2.length()+1).forEach(i-> count[0][i] = 0);
+            for(int x = 1; x <= s1.length(); x++){
+                for(int y = 1; y <= s2.length(); y++){
+                    if(s1.charAt(x-1) == s2.charAt(y-1)){
+                        count[x][y] = count[x-1][y-1] + 1;
+                        continue;
+                    }
+                    count[x][y] = Math.max(count[x-1][y], count[x][y-1]);
                 }
-                else {
-                    dp[i+1][j+1] = Math.max(dp[i+1][j], dp[i][j+1]);
+            }
+            StringBuilder sb = new StringBuilder();
+            int x = s1.length();
+            int y = s2.length();
+            while(sb.length() < count[s1.length()][s2.length()]){
+                if(s1.charAt(x-1) == s2.charAt(y-1)){
+                    sb.append(s1.charAt(x-1));
+                    x--;
+                    y--;
+                    continue;
                 }
+                if(count[x-1][y] > count[x][y-1]){
+                    x--;
+                    continue;
+                }
+                y--;
             }
+            sb.reverse();
+            System.out.println(sb.length());
+            System.out.println(sb.toString());
         }
-
-        int x = n1.length;
-        int y = n2.length;
-        String a = "";
-        while(x!=0 && y!=0) {
-
-            if(n1[x-1].equals(n2[y-1])) {
-                a +=n1[x-1];
-            }
-
-            if(dp[x-1][y] == dp[x][y]) {
-                x-=1;
-            } else if(dp[x][y-1] == dp[x][y]) {
-                y-=1;
-            } else {
-                x-=1;
-                y-=1;
-            }
-        }
-
-        String ans = "";
-        for(int i=a.length()-1; i>=0; i--) {
-            ans += a.charAt(i);
-        }
-
-
-        return ans;
-
     }
-
-
-
-
 }
