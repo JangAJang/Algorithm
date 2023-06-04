3cbc471704f4d042038f7f379e651619b541ec02
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-06-04 23:16:21 +0900

    initial commit

---
+++ 프로그래머스/프로그래머스_Lv0/src/rny_string.java
@@ -0,0 +1,8 @@
+public class rny_string {
+    //rny_string
+    class Solution {
+        public String solution(String rny_string) {
+            return rny_string.replace("m", "rn");
+        }
+    }
+}
