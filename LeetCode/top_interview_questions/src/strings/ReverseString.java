62d679034126c0f54a1184cba258192046ba5df7
JangAJang <99702271+JangAJang@users.noreply.github.com>
2023-07-03 15:06:39 +0900

    initial commit

---
+++ LeetCode/top_interview_questions/src/strings/ReverseString.java
@@ -0,0 +1,17 @@
+package strings;
+
+public class ReverseString {
+    class Solution {
+        public void reverseString(char[] s) {
+            int left = 0;
+            int right = s.length-1;
+            while(left <= right) {
+                char tmp = s[left];
+                s[left] = s[right];
+                s[right] = tmp;
+                left++;
+                right--;
+            }
+        }
+    }
+}
