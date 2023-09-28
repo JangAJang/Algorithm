ead701b10d21dd63c841fb7b53ca1931ddca796e
JangAJang <janghee5395@gmail.com>
2023-09-28 19:21:53 +0900

    Update parseCode.js

--- 프로그래머스/프로그래머스_Lv0/js_solution/parseCode.js
+++ 프로그래머스/프로그래머스_Lv0/js_solution/parseCode.js
@@ -1,19 +1,21 @@
 function solution(code) {
-  let isEvenIndex = true;
-  let result = "";
-  for (let index = 0; index < code.length; index++) {
-    if (code[index] === "1") {
-      isEvenIndex = !isEvenIndex;
-      continue;
-    }
-    if (isEvenIndex && index % 2 == 0) {
-      result += code[index];
-      continue;
-    }
-    if (!isEvenIndex && index & (2 != 0)) {
-      result += code[index];
-      continue;
-    }
-  }
-  return result;
+  let mode = 0;
+  const retCode = code
+    .split("")
+    .filter((value, idx) => {
+      if (value === "1") {
+        mode = mode === 0 ? 1 : 0;
+        return false;
+      }
+
+      // mode === 1
+      if (mode) {
+        return idx % 2 === 1;
+      }
+      // mode === 0
+      return idx % 2 === 0;
+    })
+    .join("");
+  const answer = retCode.length !== 0 ? retCode : "EMPTY";
+  return answer;
 }
