bd9ee26fde591d372c1d552e17be631805242293
JangAJang <janghee5395@gmail.com>
2023-12-20 20:32:35 +0900

    initial commit

---
+++ 프로그래머스/프로그래머스_Lv2/js/mazeRunner.js
@@ -0,0 +1,63 @@
+function solution(maps) {
+  var answer = 0;
+  return answer;
+}
+
+function BFS(graph, start, visited) {
+  const queue = new Queue();
+  queue.push(start);
+  visited[start] = true;
+
+  while (queue.size()) {
+    const v = queue.popleft();
+    console.log(v);
+
+    for (const node of graph[v]) {
+      if (!visited[node]) {
+        queue.push(node);
+        visited[node] = true;
+      }
+    }
+  }
+}
+
+class Queue {
+  constructor() {
+    this.store = {};
+    this.front = 0;
+    this.rear = 0;
+  }
+
+  size() {
+    if (this.store[this.rear] === undefined) {
+      return 0;
+    } else {
+      return this.rear - this.front + 1;
+    }
+  }
+
+  push(value) {
+    if (this.size() === 0) {
+      this.store["0"] = value;
+    } else {
+      this.rear += 1;
+      this.store[this.rear] = value;
+    }
+  }
+
+  popleft() {
+    let temp;
+    if (this.front === this.rear) {
+      temp = this.store[this.front];
+      delete this.store[this.front];
+      this.front = 0;
+      this.rear = 0;
+      return temp;
+    } else {
+      temp = this.store[this.front];
+      delete this.store[this.front];
+      this.front += 1;
+      return temp;
+    }
+  }
+}
