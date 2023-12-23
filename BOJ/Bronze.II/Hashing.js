3aee9f4bf943b4f865ebf9d1162cb3eebc4c3320
JangAJang <janghee5395@gmail.com>
2023-12-23 19:57:58 +0900

    initial commit

--- BOJ/Bronze.II/Hashing.js
+++ BOJ/Bronze.II/Hashing.js
@@ -1,33 +1,45 @@
 // 15829
+const [N, string] = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((v) => v.split(""));
 
-const readline = require("readline");
+let sum = 0n;
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-
-const input = [];
-
-rl.on("line", (line) => {
-  input.push(line);
-  if (input.length === 2) {
-    rl.close();
-  }
-});
-
-rl.on("close", () => {
-  console.log(hashFunc());
-  process.exit();
-});
-
-const hashFunc = () => {
-  const alphabets = input[1];
-  let result = BigInt(0);
-  alphabets.split("").forEach((each, index) => {
-    result += BigInt(
-      31 ** index * (each.charCodeAt(0) - "a".charCodeAt(0) + 1)
-    );
-  });
-  return String(result).replace("n", "");
+const alphabet = {
+  a: 1n,
+  b: 2n,
+  c: 3n,
+  d: 4n,
+  e: 5n,
+  f: 6n,
+  g: 7n,
+  h: 8n,
+  i: 9n,
+  j: 10n,
+  k: 11n,
+  l: 12n,
+  m: 13n,
+  n: 14n,
+  o: 15n,
+  p: 16n,
+  q: 17n,
+  r: 18n,
+  s: 19n,
+  t: 20n,
+  u: 21n,
+  v: 22n,
+  w: 23n,
+  x: 24n,
+  y: 25n,
+  z: 26n,
 };
+
+string.forEach((el, idx) => {
+  idx = BigInt(idx);
+  sum += alphabet[el] * 31n ** idx;
+});
+
+console.log(String(sum % 1234567891n));
