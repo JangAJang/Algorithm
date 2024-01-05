6200c28a48bb5c8bfec22942fa6baee047c1dd6b
JangAJang <janghee5395@gmail.com>
2024-01-05 18:18:57 +0900

    initial commit

--- 프로그래머스/프로그래머스_Lv2/js/mazeRunner.js
+++ 프로그래머스/프로그래머스_Lv2/js/mazeRunner.js
@@ -1,75 +1,83 @@
-function solution(maps) {
-  const visited = [];
-  let start;
+const X_DIRECT = [-1, 0, 1, 0];
+const Y_DIRECT = [0, -1, 0, 1];
+
+const map = [];
+let visited = [];
+let total = [];
+const start = [];
+const lever = [];
+const end = [];
+
+const createMap = (maps) => {
   for (let x = 0; x < maps.length; x++) {
-    maps[x] = maps[x].split("");
-    const temp = [];
-    for (let y = 0; y < maps[x].length; y++) {
-      if (maps[x][y] === "S") {
-        start = [x, y];
-        temp.push(true);
+    const row = maps[x].split("");
+    map.push(row);
+    for (let y = 0; y < row.length; y++) {
+      if (row[y] === "S") {
+        start.push(x);
+        start.push(y);
         continue;
       }
-      temp.push(false);
-    }
-    visited.push(temp);
-  }
-}
-
-function BFS(graph, start, visited) {
-  const queue = new Queue();
-  queue.push(start);
-  visited[start[0]][start[1]] = true;
-
-  while (queue.size()) {
-    const v = queue.popleft();
-
-    for (const node of graph[v]) {
-      if (!visited[node]) {
-        queue.push(node);
-        visited[node] = true;
+      if (row[y] === "E") {
+        end.push(x);
+        end.push(y);
+        continue;
+      }
+      if (row[y] === "L") {
+        lever.push(x);
+        lever.push(y);
+        continue;
       }
     }
   }
-}
+};
 
-class Queue {
-  constructor() {
-    this.store = {};
-    this.front = 0;
-    this.rear = 0;
-  }
+const createVisited = () => {
+  visited = map.map((each) => new Array(each.length).fill(false));
+};
 
-  size() {
-    if (this.store[this.rear] === undefined) {
-      return 0;
-    } else {
-      return this.rear - this.front + 1;
+const createResult = () => {
+  total = map.map((each) =>
+    new Array(each.length).fill(Number.MAX_SAFE_INTEGER)
+  );
+};
+
+const bfs = (start) => {
+  const queue = [];
+  queue.push(start);
+  total[start[0]][start[1]] = 0;
+  while (queue.length != 0) {
+    const [x, y] = queue.shift();
+    if (visited[x][y]) continue;
+    visited[x][y] = true;
+    for (let index = 0; index < 4; index++) {
+      const nextX = x + X_DIRECT[index];
+      const nextY = y + Y_DIRECT[index];
+      if (
+        nextX < 0 ||
+        nextX >= map.length ||
+        nextY < 0 ||
+        nextY >= map[0].length
+      )
+        continue;
+      if (map[nextX][nextY] === "X") continue;
+      total[nextX][nextY] = Math.min(total[nextX][nextY], total[x][y] + 1);
+      queue.push([nextX, nextY]);
     }
   }
+};
 
-  push(value) {
-    if (this.size() === 0) {
-      this.store["0"] = value;
-    } else {
-      this.rear += 1;
-      this.store[this.rear] = value;
-    }
-  }
-
-  popleft() {
-    let temp;
-    if (this.front === this.rear) {
-      temp = this.store[this.front];
-      delete this.store[this.front];
-      this.front = 0;
-      this.rear = 0;
-      return temp;
-    } else {
-      temp = this.store[this.front];
-      delete this.store[this.front];
-      this.front += 1;
-      return temp;
-    }
-  }
-}
+const solution = (maps) => {
+  createMap(maps);
+  createVisited();
+  createResult();
+  bfs(start);
+  const toLever = total[lever[0]][lever[1]];
+  createVisited();
+  createResult();
+  bfs(lever);
+  const toEnd = total[end[0]][end[1]];
+  return toLever >= Number.MAX_SAFE_INTEGER || toEnd >= Number.MAX_SAFE_INTEGER
+    ? -1
+    : toLever + toEnd;
+};
