b88655449227b05af79bccd4a9c69def992c49c3
JangAJang <janghee5395@gmail.com>
2024-01-13 16:30:26 +0900

    feat: 예제 성공하게 수정

--- 프로그래머스/프로그래머스_Lv3/야근 지수/야근지수.js
+++ 프로그래머스/프로그래머스_Lv3/야근 지수/야근지수.js
@@ -1,12 +1,12 @@
 function solution(n, works) {
   if (works.reduce((a, b) => a + b) <= n) return 0;
 
-  let sorted = works.sort((a, b) => b - a);
+  let sorted = works.sort((a, b) => a - b);
 
   while (n) {
-    const max = sorted[0];
+    const max = sorted[works.length - 1];
 
-    for (let i = 0; i < works.length; i--) {
+    for (let i = works.length - 1; i >= 0; i--) {
       if (sorted[i] >= max) {
         sorted[i]--;
         n--;
