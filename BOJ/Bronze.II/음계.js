34dd81304ccff8a137673d9d2b42725f784c78fa
JangAJang <janghee5395@gmail.com>
2024-01-19 11:01:45 +0900

    initial commit

--- BOJ/Bronze.II/음계.js
+++ BOJ/Bronze.II/음계.js
@@ -1,7 +1,19 @@
+const map = require("fs")
+  .readFileSync("/dev/stdin")
+  .toString()
+  .trim()
+  .split(" ")
+  .map((v) => Number(v.trim()));
+// const map = "1 2 3 4 5 6 7 8".split(" ").map((v) => Number(v.trim()));
+
 const solution = (arr) => {
-  const sorted = [...arr].sorted((a, b) => a - b);
-  const reverse = [...arr].sorted((a, b) => b - a);
-  if (arr == sorted) return "ascending";
-  if (arr == reverse) return "descending";
-  return mixed;
+  const sorted = [...arr].sort((a, b) => a - b);
+  const reverse = [...arr].sort((a, b) => b - a);
+  console.log(sorted);
+  console.log(reverse);
+  if (JSON.stringify(arr) == JSON.stringify(sorted)) return "ascending";
+  if (JSON.stringify(arr) == JSON.stringify(reverse)) return "descending";
+  return "mixed";
 };
+
+console.log(solution(map));
