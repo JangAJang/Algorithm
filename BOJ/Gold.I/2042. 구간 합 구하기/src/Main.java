6ade268f7038350681f0da764ccc78fe070533e9
JangAJang <janghee5395@gmail.com>
2024-02-26 20:16:28 +0900

    initial commit

--- BOJ/Gold.I/2042. 구간 합 구하기/src/Main.java
+++ BOJ/Gold.I/2042. 구간 합 구하기/src/Main.java
@@ -1,95 +1,82 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-
+    static long[] arr, tree;
     private static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws NumberFormatException, IOException {
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int taskCount = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
-        long[] arr = new long [n];
-        for(int index = 0; index < n; index++) {
-            arr[index] = Long.parseLong(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        arr = new long[N + 1];
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Long.parseLong(br.readLine());
         }
-        Node node = Node.createSegmentTree(arr, 1, n);
-        node.printNodeInfo();
-        while(taskCount-- > 0) {
+        tree = new long[N * 4];
+
+        init(1, N, 1);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < M + K; i++) {
             st = new StringTokenizer(br.readLine());
-            int task = Integer.parseInt(st.nextToken());
-            int firstInput = Integer.parseInt(st.nextToken());
-            if(task == 1) {
-                long secondInput = Long.parseLong(st.nextToken());
-                node.changeSum(firstInput, secondInput - arr[firstInput-1]);
-                node.printNodeInfo();
-                continue;
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            long c = Long.parseLong(st.nextToken());
+
+            if (a == 1) {
+                long dif = c - arr[b];
+                arr[b] = c;
+                update(1, N, 1, b, dif);
+            } else if (a == 2) {
+                sb.append(sum(1, N, 1, b, (int) c)).append("\n");
             }
-            int secondInput = Integer.parseInt(st.nextToken());
-            System.out.println(node.getRangeSum(firstInput, secondInput));
         }
-    }
-}
-
-class Node {
-    private long value;
-    private int start;
-    private int end;
-    private Node leftChild;
-    private Node rightChild;
-
-    public Node(long value, int start, int end) {
-        this.value = value;
-        this.start = start;
-        this.end = end;
+        System.out.println(sb.toString());
     }
 
-    public boolean containsIndex(final int index) {
-        return this.start <= index && this.end >= index;
-    }
-
-    public static Node createSegmentTree(final long[] ingredients, final int start, final int end) {
-        if(ingredients.length == 1) {
-            return new Node((ingredients[0]), start, end);
+    public static long init(int start, int end, int node) {
+        if (start == end) {
+            return tree[node] = arr[start];
         }
 
-        Node node = new Node(Arrays.stream(ingredients).sum(), start, end);
-        node.createChild(ingredients, start, end);
-        return node;
+        int mid = (start + end) / 2;
+        return tree[node] = init(start, mid, node * 2) + init(mid + 1, end, node * 2 + 1);
     }
 
-    public void createChild(final long[] ingredients, final int start, final int end) {
-        int mid = (end+start-1)/2;
-        long[] leftIngredients = Arrays.copyOfRange(ingredients, 0, ingredients.length/2);
-        long[] rightIngredients = Arrays.copyOfRange(ingredients, ingredients.length/2, ingredients.length);
-        this.leftChild = Node.createSegmentTree(leftIngredients, start, mid);
-        this.rightChild = Node.createSegmentTree(rightIngredients, mid+1, end);
+    public static long sum(int start, int end, int node, int left, int right) {
+        if (left > end || right < start) {
+            return 0;
+        }
+
+        // 범위 안에 있는 경우
+        if (left <= start && end <= right) {
+            return tree[node];
+        }
+        int mid = (start + end) / 2;
+        return sum(start, mid, node * 2, left, right) + sum(mid + 1, end, node * 2 + 1, left, right);
     }
 
-    public void printNodeInfo() {
-        if(leftChild != null) leftChild.printNodeInfo();
-        if(rightChild != null) rightChild.printNodeInfo();
+    public static void update(int start, int end, int node, int idx, long dif) {
+        // 범위 밖에 있는 경우
+        if (idx < start || idx > end) {
+            return;
+        }
+
+        tree[node] += dif;
+        if (start == end) {
+            return;
+        }
+
+        int mid = (start + end) / 2;
+        update(start, mid, node * 2, idx, dif);
+        update(mid + 1, end, node * 2 + 1, idx, dif);
     }
 
-    public long getRangeSum(final int start, final int end) {
-        if(leftChild != null && leftChild.containsIndex(start) && leftChild.containsIndex(end))
-            return leftChild.getRangeSum(start, end);
-        if(rightChild != null && rightChild.containsIndex(start) && rightChild.containsIndex(end))
-            return rightChild.getRangeSum(start, end);
-
-        if(start >= end) return this.value;
-
-        long leftSum = leftChild != null ? leftChild.getRangeSum(start, leftChild.end) : 0;
-        long rightSum = rightChild != null ? rightChild.getRangeSum(rightChild.start, end) : 0;
-        return leftSum + rightSum;
-    }
-
-    public void changeSum(int index, long value) {
-        this.value += value;
-        if(leftChild != null && leftChild.containsIndex(index)) leftChild.changeSum(index, value);
-        if(rightChild != null && rightChild.containsIndex(index)) rightChild.changeSum(index, value);
-    }
 }
