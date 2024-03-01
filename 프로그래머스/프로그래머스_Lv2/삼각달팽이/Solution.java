8bbd78d8a8f59ab862e65f08aea2b8a9b1d2fa9c
JangAJang <janghee5395@gmail.com>
2024-03-01 18:31:56 +0900

    feat: 삼각달팽이 문제를 자바로 해결

---
+++ 프로그래머스/프로그래머스_Lv2/삼각달팽이/Solution.java
@@ -0,0 +1,31 @@
+import java.lang.reflect.Array;
+
+class Solution {
+    public int[] solution(int n) {
+        int[][] numArr = new int[n+1][n+1];
+        int row = -1,
+            col = 0;
+        int curNum = 1;
+        int[] answer = new int[ n * (n-1) / 2];
+
+        for (int i = n; i > 0; i -= 3) {
+            for (int j = 0; j < i; j++) {
+            numArr[++row][col] = curNum++;
+            }
+            for (int j = 0; j < i - 1; j++) {
+            numArr[row][++col] = curNum++;
+            }
+            for (int j = 0; j < i - 2; j++) {
+            numArr[--row][--col] = curNum++;
+            }
+        }
+
+        for(int x = 1; x <=n; x++) {
+            for(int y = 1; y <=n; y++) {
+                int index = (x-1)*n + (y-1);
+                answer[index] = numArr[x][y];
+            }
+        }
+        return answer;
+    }
+}
