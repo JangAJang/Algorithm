925d61f9e622c5a7d94b89beb213d430ba9d8c41
JangAJang <janghee5395@gmail.com>
2024-03-03 15:04:01 +0900

    initial commit

--- 프로그래머스/프로그래머스_Lv1/[PCCP 기출문제 ] 1번 /solution.java
+++ 프로그래머스/프로그래머스_Lv1/[PCCP 기출문제 ] 1번 /solution.java
@@ -1,31 +1,43 @@
-class Solution {
+import java.util.HashMap;
+import java.util.Map;
+
+public class Solution {
+
+    static Map<Integer, Integer> attackInfo = new HashMap<>();
+    static int endTime = 0;
+    static int curHealth = 0;
+    static int successiveTime = 0;
+
     public int solution(int[] bandage, int health, int[][] attacks) {
-    
-    int cast_time = bandage[0]; 
-    int healing_per_second = bandage[1]; 
-    int additional_healing = bandage[2]; 
-    
-    int max_health = health; 
-    
-    int pre_attack_time = attacks[0][0];
-    
-    for(int i=0; i<attacks.length; i++){
-        int attack_time = attacks[i][0];
-        int damage = attacks[i][1];
-        int time_between = attack_time - pre_attack_time - 1;
-    if (time_between > 0){
-    
-        health += (time_between * healing_per_second);
-        health += ((time_between / cast_time) * additional_healing);
-    
-            if (health > max_health){
-                health = max_health;
+        curHealth = health;
+        for (int[] attack : attacks) {
+            attackInfo.put(attack[0], attack[1]);
+            endTime = attack[0];
+        }
+
+        /* 1초부터 마지막 공격 시각까지 체력 정보 초기화 */
+        for (int i = 1; i <= endTime; i++) {
+            if (attackInfo.containsKey(i)) { // 공격이 있다면
+                curHealth -= attackInfo.get(i);
+                successiveTime = 0;
+            } else { // 공격이 없다면
+                curHealth += bandage[1];
+                successiveTime++;
+
+                if (successiveTime == bandage[0]) {
+                    curHealth += bandage[2];
+                    successiveTime = 0;
+                }
+                if (curHealth > health) {
+                    curHealth = health;
+                }
+            }
+
+            if (curHealth <= 0) { // 이번 턴을 마치고 체력이 바닥났다면 종료
+                return -1;
             }
         }
-        health -= damage;
-        if (health <= 0) return -1;
-        pre_attack_time = attack_time;
-    }
-    return health;
+
+        return curHealth;
     }
 }
--- 프로그래머스/프로그래머스_Lv2/삼각달팽이/Solution.java
+++ 프로그래머스/프로그래머스_Lv2/삼각달팽이/Solution.java
@@ -1,12 +1,10 @@
-import java.lang.reflect.Array;
-
 class Solution {
     public int[] solution(int n) {
         int[][] numArr = new int[n+1][n+1];
         int row = -1,
             col = 0;
         int curNum = 1;
-        int[] answer = new int[ n * (n-1) / 2];
+        int[] answer = new int[ n * (n-1) / 2 + 1];
 
         for (int i = n; i > 0; i -= 3) {
             for (int j = 0; j < i; j++) {
