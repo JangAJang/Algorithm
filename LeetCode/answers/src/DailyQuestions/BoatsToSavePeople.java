a32f3f3f60f45d7b00d3bacf97c9502d652e9c0b
JangAJang <janghee5395@gmail.com>
2024-05-04 13:18:50 +0900

    Create BoatsToSavePeople.java

---
+++ LeetCode/answers/src/DailyQuestions/BoatsToSavePeople.java
@@ -0,0 +1,24 @@
+package DailyQuestions;
+
+import java.util.Arrays;
+
+public class BoatsToSavePeople {
+
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+
+        int left = 0;
+        int right = people.length - 1;
+        int boats = 0;
+
+        while (left <= right) {
+            if (people[left] + people[right] <= limit) {
+                left++;
+            }
+            right--;
+            boats++;
+        }
+
+        return boats;
+    }
+}
