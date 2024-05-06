2dc5f2a746605498c25b48e4ec692d7a24b6dc21
JangAJang <janghee5395@gmail.com>
2024-05-06 13:38:16 +0900

    Create RemoveNodesFromLinkedList.java

---
+++ LeetCode/answers/src/DailyQuestions/RemoveNodesFromLinkedList.java
@@ -0,0 +1,27 @@
+package DailyQuestions;
+
+public class RemoveNodesFromLinkedList {
+
+    public ListNode removeNodes(ListNode head) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        while(head != null) {
+            if(deque.isEmpty() || deque.getFirst() >= head.val) {
+                deque.addFirst(head.val);
+                head = head.next;
+                continue;
+            }
+
+            while(!deque.isEmpty() && deque.getFirst() < head.val) {
+                deque.removeFirst();
+            }
+            deque.addFirst(head.val);
+            head = head.next;
+        }
+
+        ListNode root = new ListNode(deque.removeFirst());
+        while(!deque.isEmpty()) {
+            root = new ListNode(deque.removeFirst(), root);
+        }
+        return root;
+    }
+}
