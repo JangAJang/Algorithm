36807c04b29ba4f246027c2f32cc53bb84dda3d2
JangAJang <janghee5395@gmail.com>
2024-05-07 11:18:53 +0900

    Create AddTwoNumbers.java

---
+++ LeetCode/answers/src/DailyQuestions/AddTwoNumbers.java
@@ -0,0 +1,24 @@
+package DailyQuestions;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class AddTwoNumbers {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        Deque<Integer> deque = new ArrayDeque<>();
+        int ascender = 0;
+        while(l1 != null || l2 != null || ascender != 0) {
+            int value = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + ascender;
+            deque.addLast(value % 10);
+            ascender = value / 10;
+            if(l1 != null) l1 = l1.next;
+            if(l2 != null) l2 = l2.next;
+        }
+        ListNode answer = null;
+        while(!deque.isEmpty()) {
+            answer = new ListNode(deque.removeLast(), answer);
+        }
+        return answer;
+    }
+}
