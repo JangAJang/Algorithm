dcb09097a99ee59fb82f8cbe9e1d7bbdcf524fa9
JangAJang <janghee5395@gmail.com>
2024-06-03 12:10:11 +0900

    Create AppendCharactersToStringToMakeSubsequence.java

---
+++ LeetCode/answers/src/DailyQuestions/AppendCharactersToStringToMakeSubsequence.java
@@ -0,0 +1,14 @@
+package DailyQuestions;
+
+public class AppendCharactersToStringToMakeSubsequence {
+
+    public int appendCharacters(String s, String t) {
+        int tIndex = 0;
+        for(int index = 0; index < s.length(); index++) {
+            if(tIndex < t.length() && s.charAt(index) == t.charAt(tIndex)) {
+                tIndex++;
+            }
+        }
+        return t.length() - tIndex;
+    }
+}
