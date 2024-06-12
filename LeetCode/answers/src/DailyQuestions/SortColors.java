c90ecd399090acbd3bff7ffe2c9be17792b82dc8
JangAJang <janghee5395@gmail.com>
2024-06-12 09:53:42 +0900

    initial commit

---
+++ LeetCode/answers/src/DailyQuestions/SortColors.java
@@ -0,0 +1,23 @@
+package DailyQuestions;
+
+public class SortColors {
+
+    public void sortColors(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int each : nums) {
+            map.put(each, map.getOrDefault(each, 0)+1);
+        }
+
+        int index = 0;
+
+        for(int key = 0; key < 3; key++) {
+            if(!map.containsKey(key)) continue;
+            int value = map.get(key);
+            while(value-- > 0) {
+                nums[index] = key;
+                index++;
+            }
+        }
+    }
+}
