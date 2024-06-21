4bbfa0dac3d2a8a6365030d572482377c13c70a5
JangAJang <janghee5395@gmail.com>
2024-06-21 09:52:18 +0900

    Create GrumpyBookstoreOwner.java

---
+++ LeetCode/answers/src/DailyQuestions/GrumpyBookstoreOwner.java
@@ -0,0 +1,32 @@
+package DailyQuestions;
+
+public class GrumpyBookstoreOwner {
+
+    public int maxSatisfied(int[] customers, int[] grumpy, int minutes) {
+        int n = customers.length;
+        int totalSatisfied = 0;
+        for (int i = 0; i < n; i++) {
+            if (grumpy[i] == 0) {
+                totalSatisfied += customers[i];
+            }
+        }
+        int addSatisfied = 0;
+        int currWindowSatisfied = 0;
+        for (int i = 0; i < minutes; i++) {
+            if (grumpy[i] == 1) {
+                currWindowSatisfied += customers[i];
+            }
+        }
+        addSatisfied = currWindowSatisfied;
+        for (int i = minutes; i < n; i++) {
+            if (grumpy[i] == 1) {
+                currWindowSatisfied += customers[i];
+            }
+            if (grumpy[i - minutes] == 1) {
+                currWindowSatisfied -= customers[i - minutes];
+            }
+            addSatisfied = Math.max(addSatisfied, currWindowSatisfied);
+        }
+        return totalSatisfied + addSatisfied;
+    }
+}
