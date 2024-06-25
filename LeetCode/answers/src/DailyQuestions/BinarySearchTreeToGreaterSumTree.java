8e1683c8de3e30e2a475a89be78e338f5f8eadf4
JangAJang <janghee5395@gmail.com>
2024-06-25 20:05:54 +0900

    Create BinarySearchTreeToGreaterSumTree.java

---
+++ LeetCode/answers/src/DailyQuestions/BinarySearchTreeToGreaterSumTree.java
@@ -0,0 +1,16 @@
+package DailyQuestions;
+
+public class BinarySearchTreeToGreaterSumTree {
+
+    private int sum = 0;
+
+    public TreeNode bstToGst(TreeNode root) {
+        if (root != null) {
+            bstToGst(root.right);
+            sum += root.val;
+            root.val = sum;
+            bstToGst(root.left);
+        }
+        return root;
+    }
+}
