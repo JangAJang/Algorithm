2ce890297345f313216c932ae46a9b123c756c30
JangAJang <janghee5395@gmail.com>
2024-07-13 13:41:33 +0900

    initial commit

---
+++ BOJ/Bronze.IIII/ABC/Main.java
@@ -0,0 +1,14 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.next();
+        String b = sc.next();
+        String c = sc.next();
+        System.out.println(Integer.parseInt(a) + Integer.parseInt(b) - Integer.parseInt(c));
+        System.out.println(Integer.parseInt(a + b) - Integer.parseInt(c));
+    }
+}
