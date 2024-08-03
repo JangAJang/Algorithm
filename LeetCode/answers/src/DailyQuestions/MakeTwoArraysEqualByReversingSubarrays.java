01b4edb4b7f6776991f8aef02bb29dcbee16d92b
JangAJang <janghee5395@gmail.com>
2024-08-03 22:52:45 +0900

    Create MakeTwoArraysEqualByReversingSubarrays.java

---
+++ LeetCode/answers/src/DailyQuestions/MakeTwoArraysEqualByReversingSubarrays.java
@@ -0,0 +1,16 @@
+package DailyQuestions;
+
+public class MakeTwoArraysEqualByReversingSubarrays {
+
+    public boolean canBeEqual(int[] target, int[] arr) {
+        int[] cnt1 = new int[1001];
+        int[] cnt2 = new int[1001];
+        for (int v : target) {
+            ++cnt1[v];
+        }
+        for (int v : arr) {
+            ++cnt2[v];
+        }
+        return Arrays.equals(cnt1, cnt2);
+    }
+}
