9c5c7fcd57071207ddabfbdcc63595e1ff00fcb9
JangAJang <janghee5395@gmail.com>
2024-08-25 16:14:27 +0900

    Create BinaryTreePostorderTraversal.java

---
+++ LeetCode/answers/src/DailyQuestions/BinaryTreePostorderTraversal.java
@@ -0,0 +1,18 @@
+package DailyQuestions;
+
+public class BinaryTreePostorderTraversal {
+
+    public void traversal(TreeNode root, List<Integer> res){
+        if (root != null){
+            traversal(root.left, res);
+            traversal(root.right, res);
+            res.add(root.val);
+        }
+    }
+
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        traversal(root, res);
+        return res;
+    }
+}
