44b65c01937fe4c0c00236fb0b04cf10dbfe4452
JangAJang <janghee5395@gmail.com>
2024-09-06 20:32:09 +0900

    initial commit

---
+++ LeetCode/answers/src/DailyQuestions/DeleteNodesFromLinkedListPresentInArray.java
@@ -0,0 +1,30 @@
+package DailyQuestions;
+
+public class DeleteNodesFromLinkedListPresentInArray {
+
+    class Solution {
+        public ListNode modifiedList(int[] nums, ListNode head) {
+            int max = -1;
+            for(int num : nums ){
+                max = num > max ? num : max;
+            }
+            boolean[] freq = new boolean[max+1];
+
+            for(int num : nums) freq[num] = true;
+
+            ListNode temp = new ListNode();
+            ListNode current = temp;
+
+            while(head != null){
+                if( head.val >= freq.length || freq[head.val] == false){
+                    current.next = head;
+                    current = current.next;
+                }
+                head = head.next;
+            }
+
+            current.next = null;
+            return temp.next;
+        }
+    }
+}
