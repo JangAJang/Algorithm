4b329156f3a4294f63d4cc7561fa7ac1aed560d5
JangAJang <janghee5395@gmail.com>
2024-09-07 20:56:26 +0900

    Create LinkedListInABinaryTree.java

---
+++ LeetCode/answers/src/DailyQuestions/LinkedListInABinaryTree.java
@@ -0,0 +1,18 @@
+package DailyQuestions;
+
+public class LinkedListInABinaryTree {
+
+    public boolean isSubPath(ListNode head, TreeNode root) {
+        return dfs(head, head, root);  // Start DFS search
+    }
+
+    // DFS helper function
+    boolean dfs(ListNode head, ListNode cur, TreeNode root) {
+        if (cur == null) return true;
+        if (root == null) return false;
+        if (cur.val == root.val) cur = cur.next;
+        else if (head.val == root.val) head = head.next;
+        else cur = head;
+        return dfs(head, cur, root.left) || dfs(head, cur, root.right);
+    }
+}
