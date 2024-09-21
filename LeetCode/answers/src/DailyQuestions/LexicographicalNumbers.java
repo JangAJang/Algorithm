0d470109a6373fd244a6807c3f04e5ccd400795c
JangAJang <janghee5395@gmail.com>
2024-09-21 21:13:52 +0900

    Create LexicographicalNumbers.java

---
+++ LeetCode/answers/src/DailyQuestions/LexicographicalNumbers.java
@@ -0,0 +1,29 @@
+package DailyQuestions;
+
+public class LexicographicalNumbers {
+
+    public List<Integer> lexicalOrder(int n) {
+        List<Integer> lexicographicalNumbers = new ArrayList<>();
+        for (int start = 1; start <= 9; ++start) {
+            generateLexicalNumbers(start, n, lexicographicalNumbers);
+        }
+        return lexicographicalNumbers;
+    }
+
+    private void generateLexicalNumbers(
+        int currentNumber,
+        int limit,
+        List<Integer> result
+    ) {
+        if (currentNumber > limit) return;
+        result.add(currentNumber);
+        for (int nextDigit = 0; nextDigit <= 9; ++nextDigit) {
+            int nextNumber = currentNumber * 10 + nextDigit;
+            if (nextNumber <= limit) {
+                generateLexicalNumbers(nextNumber, limit, result);
+            } else {
+                break;
+            }
+        }
+    }
+}
