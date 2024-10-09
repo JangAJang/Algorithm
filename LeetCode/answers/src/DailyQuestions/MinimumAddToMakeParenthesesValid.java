88ee0d3c3d885ad8e0c740041ac807f3a6fd09d9
JangAJang <janghee5395@gmail.com>
2024-10-09 23:49:27 +0900

    Create MinimumAddToMakeParenthesesValid.java

---
+++ LeetCode/answers/src/DailyQuestions/MinimumAddToMakeParenthesesValid.java
@@ -0,0 +1,21 @@
+package DailyQuestions;
+
+public class MinimumAddToMakeParenthesesValid {
+
+    public int minAddToMakeValid(String s) {
+        int open =0, mismatch=0;
+        for(int i=0; i<s.length(); i++)
+        {
+            if(s.charAt(i)=='(')
+                open++;
+            else
+            {
+                if(open>0)
+                    open--;
+                else
+                    mismatch++;
+            }
+        }
+        return open+mismatch;
+    }
+}
