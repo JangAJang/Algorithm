b157f1093104a17a419be4bc600c96652b73cc9a
JangAJang <janghee5395@gmail.com>
2024-10-10 13:52:56 +0900

    Create MaximumWidthRamp.java

---
+++ LeetCode/answers/src/DailyQuestions/MaximumWidthRamp.java
@@ -0,0 +1,30 @@
+package DailyQuestions;
+
+public class MaximumWidthRamp {
+
+    public int maxWidthRamp(int[] nums) {
+        int low = 1, high = nums.length-1, res = 0;
+
+        while(low <= high) {
+            int mid = low + (high - low)/2;
+            if(possible(nums, mid)) {
+                res = mid;
+                low = mid + 1;
+                continue;
+            }
+            high = mid - 1;
+        }
+        return res;
+    }
+
+    private boolean possible(int[] nums, int width) {
+        int i=0,j=width;
+        int min = nums[i];
+        while(j < nums.length) {
+            if(nums[j] >= min) return true;
+            j++;
+            min = Math.min(min, nums[++i]);
+        }
+        return false;
+    }
+}
